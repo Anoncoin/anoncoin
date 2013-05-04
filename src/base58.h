6a8b58179429cc02bfebe689d63ca081d9cfa29f
Meeh <meeh@i2pmail.org>
2013-05-04 03:56:29 +0200

    Adding the 'A' prefix to addresses

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 45,
+        PUBKEY_ADDRESS = 23,
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
