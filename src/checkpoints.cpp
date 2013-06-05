8be9ffabbbb211246dc3a9404ae6e0dde24f0117
Meeh <meeh@i2pmail.org>
2013-06-05 20:21:24 +0200

    Adding more checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,12 +24,14 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (     1, uint256("0x8fe2901fc0999bc86ea2668c58802ee87165438166d18154f1bd4f917bf25e0f"))
-        (     7, uint256("0x4530df06d98fc77d04dab427630fc63b45f10d2b0ad3ad3a651883938986d629"))
+        (       1, uint256("0x8fe2901fc0999bc86ea2668c58802ee87165438166d18154f1bd4f917bf25e0f"))
+        (       7, uint256("0x4530df06d98fc77d04dab427630fc63b45f10d2b0ad3ad3a651883938986d629"))
         (     42, uint256("0x6a4fbf1b3d569ff01ceaedd7b9447a8f13c815cbe7024c4f6f6865d2428aa603"))
         (     77, uint256("0x00c714c55e396cd7a39d824aae8438e5633e62a4a0e56461e792ed3254731e03"))
-        (     240, uint256("0xb6bb92a3db8b35987da49e79a0a445ff474b131d5216f85e79044edd5e0bb1a1"))
-        (     420, uint256("0x51a8f5e247354b8c39632b984970f2c7c006b341524da3668d1a9a4a94fb2a32"))
+        (   240, uint256("0xb6bb92a3db8b35987da49e79a0a445ff474b131d5216f85e79044edd5e0bb1a1"))
+        (   420, uint256("0x51a8f5e247354b8c39632b984970f2c7c006b341524da3668d1a9a4a94fb2a32"))
+        (   777, uint256("0x13682ff40afe0a643f574efe41de35453655f84644aa31fc78657c47edc3afb8"))
+        ( 1000, uint256("0x21bd2ccfbabce89927e58eb946c84cf243630c52ec4bf88b3c0fd912e58573ea"))
        ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
