36b1b6167287b8668761cd812abbd2e56aa763e3
Meeh <meeh@i2pmail.org>
2013-06-11 00:50:58 +0200

    Correcting a bug

--- src/main.cpp
+++ src/main.cpp
@@ -842,8 +842,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 420 * 205.2; // Anoncoin: 420 blocks
-static const int64 nTargetSpacing = 3.42 * 60; // Anoncoin: 3.42 minutes
+static const int64 nTargetTimespan = 86100;//420 * 205.2; // Anoncoin: 420 blocks
+static const int64 nTargetSpacing = 205;//3.42 * 60; // Anoncoin: 3.42 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
