b2c08efa75125cff2d002e8adc10d98e497ac33a
Gnosis #anoncoin Freenode IRC <gnosis@anoncoin.net>
2014-04-27 17:38:14 -0700

    fix tabs/spaces and removed leftover comment

--- src/libzerocoin/PrintUFOCandidate.cpp
+++ src/libzerocoin/PrintUFOCandidate.cpp
@@ -1,15 +1,3 @@
-/**
-* @file       tutorial.cpp
-*
-* @brief      Simple tutorial program to illustrate Zerocoin usage.
-*
-* @author     Ian Miers, Christina Garman and Matthew Green
-* @date       June 2013
-*
-* @copyright  Copyright 2013 Ian Miers, Christina Garman and Matthew Green
-* @license    This project is released under the MIT license.
-**/
-
 using namespace std;
 
 #include <string>
@@ -24,8 +12,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	for (int i = 1; i < argc; i++) {
-    uint32_t ufoIndex = strtol(argv[i], NULL, 10);
-    Bignum ufo = libzerocoin::calculateRawUFO(ufoIndex, 3840);
+		uint32_t ufoIndex = strtol(argv[i], NULL, 10);
+		Bignum ufo = libzerocoin::calculateRawUFO(ufoIndex, 3840);
 		cout << ufoIndex << ":" << ufo.ToString(10) << endl;
 	}
 	return 0;
