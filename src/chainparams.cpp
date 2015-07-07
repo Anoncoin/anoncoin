f396f4ab1ce4b81aadf5b6f3faf8b23924203390
GroundRod <RodGround@gmail.com>
2015-07-07 17:31:01 -0600

    I2P Seed node updates

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -25,9 +25,18 @@ unsigned int pnSeed[] =
 };
 
 #ifdef ENABLE_I2PSAM
-static const string I2pSeedAddresses[] = {
-    "8w-NRnXaxoGZdyupTXX78~CmF8csc~RhJr8XR2vMbNpazKhGjWNfRjhCmwqXkkW9vwkNjovW2AAbof7PfVnMCff0sHSxMTiBNsH8cuHJS2ckBSJI3h4G4ffJLc5gflangrG1raHKrMXCw8Cn56pisx4RKokEKUYdeEPiMdyJUO5yjZW2oyk4NpaUaQCqFmcglIvNOCYzVe~LK124wjJQAJc5iME1Sg9sOHaGMPL5N2qkAm5osOg2S7cZNRdIkoNOq-ztxghrv5bDL0ybeC0sfIQzvxDiKugCrSHEHCvwkA~xOu9nhNlUvoPDyCRRi~ImeomdNoqke28di~h2JF7wBGE~3ACxxOMaa0I~c9LV3O7pRU2Xj9HDn76eMGL7YCcCU4dRByu97oqfB3E~qqmmFp8W1tgvnEAMtXFTFZPYc33ZaCaIJQD7UXcQRSRV7vjw39jhx49XFsmYV3K6~D8bN8U4sRJnKQpzOJGpOSEJWh88bII0XuA55bJsfrR4VEQ5AAAA",
-    "hL51K8bxVvqX2Z4epXReUYlWTNUAK-1XdlbBd7e796s2A3icCQRhJvGlaa6PX~tgPvos-2IJp9hFQrVa0lyKyYmpQN9X7GOErtsL-JbMQpglQsEd94jDRAsiBuvgyPZij~NNdBxKRMDvNm9s7eovzhEFTAimTSB-sgeZ4Afxx2IrNXDFM6KS8AUm8YsaMldzX9zKQDeuV0slp4ZfIAVQhZZy9zTZSmUNPnXQR7XPh5w7FkXzmKMTKSyG~layJ3AorQWqzZXmykzf4z3CE4zkzQcwc0~ZIcAg9tYvM2AdQIdgeN6ISMim6L8q6ku6abuONkyw-NJTi3NopeGHZva21Tc3uHetsKoW434N24HBVUtIjJVjGsbZ7xBfz2xM5kyhPl6SlD-RJarCw47Rovmfc9Piq6q3S~Zw-rvRl-xDMJzwraIYNjAROouDwjI9Bqnguq9DH5uFBxf4uN69X7T~yWTAjdvelZKp6BGe~HGo7bNQjBmymhlH4erCKZEXOaxDAAAA"
+// As of March 30, 2015
+static const string I2pDestinationSeeds[] = {
+    "HNaqLnETmOcfEzbmzyh5XTQJCqxHVBk0mdvAbc2CZbIi5OxelNMREYLo1U2wGIOgNFSgfdvefOroouRpC8vsoXJ2ARYFTGH~wKd4KVv6sEdD3bVhtVYW03nuwz6KP9~otTO6QvX2uRSQNJlNGqGN7L4uNgzHuznU0jpCmpARDC4y95144iI7hWawxFnqthnzEgPNSGp5lqM7GQzDplWTDDAoF1GcJqPzVY0uoMWvLCfCNUpHw2wO0egKuAlv6Uz9IK0dQl7S0XmE9Jnt7I2ZKs4-RlX9lRf-hEHgMY9Y6k-Brl-TVwTkzmsvNPYTpbOxvkXIBru5Yi~IaGsImjhmvGT4FAj02b-0HNqGWfaiKOHl1k~O8geUeIDO2fA5oyJQmcwstMjmU24DMFeCBCB0MFL0xK8bJkUZkrzhwIY-Z2U9GqxEjt53m80-PHvUsDOGAKOpeT0fnx3vLVEJFeHS6K~G2XG24EzcmSCLwcPEFptQpxWrR4nSxya6KTPp6ZUCAAAA",
+    "3bfyLFHqvqCZuAtVLIR8R4buAW3e1VQ6-S6jKrXNX5Hgq-b1fYbLqniP5MjErFzhTlu-Umk9n-E9jnLsTXJQNkZB5RkNnSMSkxPsQpFWJ8pbN-qXQf1KtJ0UYEA0zi7Eq33caToH5CtGkMvik5zOFtPKQEIbgzzu6N7tvb2huGvpBTJQdTdQSI1hjfQbpnTkZ-bsJrCWs54nu13OAi~AuSvKJtGl7sLrA1z~7YyJfA84pOLO5oMxzMJnjhARMJFTyG6RxHOeTHOlNqoLVCY7sLI2IZpJrQAJLRHTTFbUKptuj4v3W8lcZDFXa4PmK-xA6NuicFC~b6YaOMEiAL1E7w72pHTwcIxmKP1FF3U57bwDGU7126n3P8PECouG0o6QXlTsYHt~Qc4pZGy4CQtINMFcx2ik0KKxOACTu8XzJ2~7FHINgMzPnd87vNK6mfBDYZ3Np~XwEZLF1GkbYYqLVKvh8NdWiqNBt5zzR0wq3XKTxCljpIs9JIP2FNLaAh0KAAAA",
+    "y2arPA7vC36rA-SrPb2spKxvhNeCvx7ZMhdO-F9csEvWYxvx1a5NK0K2Qfo0cfwh3KrNphYudAGqYfwhi~a4xUnFtZ1-ruCtQ33i298HnnDeRJv0T1~Y9dlvlN9xQnBggH2EjVv8~I1p2W2GyGliPZ0NUzjgdmX0N1jNCP5tJ1lMUqsJrdvGi~bEmbMDOA7y6-pnyrfRC6lk7qlIuT~kJh8l-HerJ0gEzu0ByZ~YDTK4EpWaCQgW68gKIgK3pKnscfILkiZwiRHkG5kWugBeJixIO3MPOAW17HdVG43EzLAlnC1BW8vkUXKI37Ry4cfylCz-90HPfRmwom-E3ZHiEoHQFAc43l9f2~KZzmtj-7u21~e47daGnoMGjta8ADwekmd-MieVFQeJ27fsRiCQkLDTq63ONbcTqIulXGhffXZtkyoq0qo-eVl3ErS6YyVpnhvvkuP46Gh81Db4zWbu~NEQq~qb1X0JhaaiufVTY2pNgHbNVinJjyjzq2ip3dGMAAAA",
+    "d-Puxd~OdmFGywsIE4cIpbCfZLId8xWJEeelq18VgnZykEYiPS0K0C9w7eb2UMfeqamYejmODi9ITePyZpyOT3vRgKR~I88AiQbiH1YjiR17-R7KDRJQXmbKfOqMpEMW7JUh-hZBI87PNkC4TTv65lSpKyt8Kz~SaX21jVEMX34klJg7ZI48JIaSvRXWFPCZoyadKOryjPIS-tqVQyX4yEbVcypLfouJ02uRYvo2SAyluDBpZ71Rn5FpnlDKkLx0srh5k6CJYMz7vozNYUub9xvKEZK0SLcKsLzy~FRyuHEbOHUZ76~ibvxUUWORt4hHdT3O-WDre17pMDnuEG8YDGHPotX-Y7CcfH-fXRacVLwhK4Y5k9BvdmiuaZZoU3GxOIGxyMGeK5MJhinNBNdZpyEvUnFPpfu7R33offpE50vKmnwim9YZVa76VG3xlRpYWu9Gj2IAfzR-3JSTvtxFPqMDHR2-D-MOeOp98Qe0CtHcAvx3kHhuCJW4gbwOJa2TAAAA",
+    "UmCjD3EY6cEeCv5N9b9cRWBkU8~EwacG7nY6Tx9vPCne5fP-LIMxR~42hyrPGN4-ZXpGbns5Pu4JEvckj~mkXnJxM2-b6An2QsVYJCCqBmnqjK43HZC32s0Z2qFW5Y3agNs3eAtSK~GXy~IvQYewhSlmIOs8W~eAkLzF~C2~VzSmaWs55uBzLUINeJr7E7woPFnnrP8EjfMJHNUbmTkw1bepf3K7~BWy-LBlBaWvylTtRiAPDlqI2Q9ZZUn3wYQbHLZSGHkjdouRJ3zndcFTjwTEHzh4XQcHQK9hud7USUGnasl1IXsxizJo5lzsP5SvytkzdI07FjhpxSAYDKHn6SW4UXM5rQmAq44zHu4AzY76p3Wk9iUtwz1rFQNLqTwouZYe5hviiB1GAaWZBOhz9KsG0DzxeFRV5ou8Ps8-xlhU36mjrw3L6CYEpT6WejbNnXlIC~aUI057MAjr2sLfvVBhmycwJdSMOIT5fVkm8aTmiIM-YgpYUpInNHxH5fKEAAAA",
+    "rR~bh73tiGezgHUs82cKHX83z~Jmp7pfyTWbwjrQgzM3at-DUh7TVQnsB5FnfJrbH89P1zXTdTqUR~OjQnjlt1WZZISrBVoUNe0O8xruhUsGR77WmRbSrb1K1o6LG0eX9dZk7ZTrLa-Bj0xG3jk8IYOLOdkCjld7VzBdkTcGN7G8QWx33cJn2778i9gSGNza0egyiEd4QnCjgZlI0wYTyLz-35F~to9unN6uq8AMK9uJa8a67l~HyDl1dGC5Ye~KKRBWgD5ZCoJAJBOipjsdAZqHUTa64xX4cX6EPEVBdikAZrRQS2l-FyLcuHvXFQ~rRuOEfO0VGmhJOIzNMdvK4UfsT118kt~pPe1nI4GEQ1xWP5thcFv-WN4mtDzU4fqFivhb7hijygUhQ7zrZFCbDypzYZTPBSXjABknbcz8j0iPy9GgEHsF8Zltt~i77Vc2e8cTcNMSpxm1uvMFJiB373Lri2vGj5oB0apixV-HUj1Bdpb1HTm96PDpf963jhj~AAAA",
+    "6Njy3LSSCG4KrIeEubXoyWIw~FIKIVesPuzWjNTyNxwmvK65udJCmvTuQyrkPNkARZDNE4-pL4aAI4EhOsWzE3LkSOsvQTG4yhzRE7r97Fg0x~nE50SnrqWoJrsGHUcJtq-wL77WSzdcMD-aA5z4fROKwf3kvXCiOcRu9V9VdOlPuiynl838zOIOqApIFUfKzNcKMvEGfa9D998dL2FI-QnNgjLTcYn1YIK7muNSrhBgDJIfZPZRi21K-Q4fUSXRyutx6pP~ipLmsCJZOFrf-y0GThoSLy2DGNJ77s~UB~ANyuglLFvqDWqKp8KOuU4GTpmEfLPrurKr2kXc3jbqmw3YLVonJev-wcH2boS9WLcODtyytzQLBfz83bMACI-YbBQESjD1dZAAR3zDztKzdQcxzbyMGuoJe-6~QrEy~35TPL2TKIr2BjTWNwt3HQ65NFHLsA7HneiLf0Ntr~wcewxK7bRY3~qI7J7oUsf-QhyjkKqHJ6ohmAMTY1U3CT3ZAAAA",
+    "kvGu5BRTohvsGsNqkQW5glv-Q1Zxzwm40mFWK9g8Jte7561SmQxkKrklKX1bfuoCQ9hn1Ty3406-7xQb7bhoD9YC1mPm9hoBDNBzb4f8jn~Zry4SUY~AfCSvD1EBW4cvme9VZ1mIu3Jtj9qyv7j0gKgGyXAkX55gXT5EhRUoWspsWxao18PsB-HUTIyMP-At8GOcAeoI4GXbFSCa170o6pggoJrsXfg28Iww1VJB7MQl5IE2Q~i-WGuynKjmVUFshH9tC-vIW0nvaY-vnxNL7GNVBHVmcyi0I3nYxym~74BfdEioV-c5Q20aztYzxgvTelJu-56AMTP9GJKnn9BjVhKs2jppZLd6WFs30wGZMoK3a7Q8~kRcHKXX82pGisnBUxAbFQyOhAUOMw2wvKUeHh6-WBE8WU6zCALyLJ2532tvf8Bb~k0S7ZD96vgaMGGiR2daHcFf1e~zmVILPf27ya8bg874HZoq7WcrUsF5tLha2cMHFx1RXwMAAPlL3mYVAAAA"
+//    "8w-NRnXaxoGZdyupTXX78~CmF8csc~RhJr8XR2vMbNpazKhGjWNfRjhCmwqXkkW9vwkNjovW2AAbof7PfVnMCff0sHSxMTiBNsH8cuHJS2ckBSJI3h4G4ffJLc5gflangrG1raHKrMXCw8Cn56pisx4RKokEKUYdeEPiMdyJUO5yjZW2oyk4NpaUaQCqFmcglIvNOCYzVe~LK124wjJQAJc5iME1Sg9sOHaGMPL5N2qkAm5osOg2S7cZNRdIkoNOq-ztxghrv5bDL0ybeC0sfIQzvxDiKugCrSHEHCvwkA~xOu9nhNlUvoPDyCRRi~ImeomdNoqke28di~h2JF7wBGE~3ACxxOMaa0I~c9LV3O7pRU2Xj9HDn76eMGL7YCcCU4dRByu97oqfB3E~qqmmFp8W1tgvnEAMtXFTFZPYc33ZaCaIJQD7UXcQRSRV7vjw39jhx49XFsmYV3K6~D8bN8U4sRJnKQpzOJGpOSEJWh88bII0XuA55bJsfrR4VEQ5AAAA",
+//    "hL51K8bxVvqX2Z4epXReUYlWTNUAK-1XdlbBd7e796s2A3icCQRhJvGlaa6PX~tgPvos-2IJp9hFQrVa0lyKyYmpQN9X7GOErtsL-JbMQpglQsEd94jDRAsiBuvgyPZij~NNdBxKRMDvNm9s7eovzhEFTAimTSB-sgeZ4Afxx2IrNXDFM6KS8AUm8YsaMldzX9zKQDeuV0slp4ZfIAVQhZZy9zTZSmUNPnXQR7XPh5w7FkXzmKMTKSyG~layJ3AorQWqzZXmykzf4z3CE4zkzQcwc0~ZIcAg9tYvM2AdQIdgeN6ISMim6L8q6ku6abuONkyw-NJTi3NopeGHZva21Tc3uHetsKoW434N24HBVUtIjJVjGsbZ7xBfz2xM5kyhPl6SlD-RJarCw47Rovmfc9Piq6q3S~Zw-rvRl-xDMJzwraIYNjAROouDwjI9Bqnguq9DH5uFBxf4uN69X7T~yWTAjdvelZKp6BGe~HGo7bNQjBmymhlH4erCKZEXOaxDAAAA"
 };
 #endif // ENABLE_I2PSAM
 
@@ -103,24 +112,22 @@ public:
 
 #ifdef ENABLE_I2PSAM
         /**
-         * ToDo: Brought all the seed data from 0.8.5.6 'master' net.cpp file, and rebuilt it for use here in chainparams...
-         *       See net.cpp->ThreadDNSAddressSeed() for how these are used.
+         *  See net.cpp->ThreadDNSAddressSeed() for how these are used.
          */
-        // As of 12/30/2014, these following entries were not working.  Renewing tests for awhile, or they need to be removed or updated...
-        i2pvSeeds.push_back(CDNSSeedData("d46o5wddsdrvg2ywnu4o57zeloayt7oyg56adz63xukordgfommq.b32.i2p", "d46o5wddsdrvg2ywnu4o57zeloayt7oyg56adz63xukordgfommq.b32.i2p"));
-        i2pvSeeds.push_back(CDNSSeedData("u2om3hgjpghqfi7yid75xdmvzlgjybstzp6mtmaxse4aztm23rwq.b32.i2p", "u2om3hgjpghqfi7yid75xdmvzlgjybstzp6mtmaxse4aztm23rwq.b32.i2p"));
-        i2pvSeeds.push_back(CDNSSeedData("htigbyeisbqizn63ftqw7ggfwfeolwkb3zfxwmyffygbilwqsswq.b32.i2p", "htigbyeisbqizn63ftqw7ggfwfeolwkb3zfxwmyffygbilwqsswq.b32.i2p"));
-        i2pvSeeds.push_back(CDNSSeedData("st4eyxcp73zzbpatgt26pt3rlfwb7g5ywedol65baalgpnhvzqpa.b32.i2p", "st4eyxcp73zzbpatgt26pt3rlfwb7g5ywedol65baalgpnhvzqpa.b32.i2p"));
-        i2pvSeeds.push_back(CDNSSeedData("qgmxpnpujddsd5ez67p4ognqsvo64tnzdbzesezdbtb3atyoxcpq.b32.i2p", "qgmxpnpujddsd5ez67p4ognqsvo64tnzdbzesezdbtb3atyoxcpq.b32.i2p"));
         i2pvSeeds.push_back(CDNSSeedData("7zbwzykhyjcmmessswamkxfyya7hioiy2oq7voaw27625qwruqia.b32.i2p", "7zbwzykhyjcmmessswamkxfyya7hioiy2oq7voaw27625qwruqia.b32.i2p")); // lunokhod's seednode
+        i2pvSeeds.push_back(CDNSSeedData("b7ziruwpk7g2e44xyomnc2nu5tx7bc2f2ai4dzi66uxm3bc3qttq.b32.i2p", "b7ziruwpk7g2e44xyomnc2nu5tx7bc2f2ai4dzi66uxm3bc3qttq.b32.i2p")); // K1773R's seednode (dnsseed01)
         i2pvSeeds.push_back(CDNSSeedData("ypwvq7jcu3uwyg4ufjqt4a26ca6pxdcnshv6q2okmmjsof5dxzkq.b32.i2p", "ypwvq7jcu3uwyg4ufjqt4a26ca6pxdcnshv6q2okmmjsof5dxzkq.b32.i2p")); // keystroke's seednode
 
-        // Has been working well with tests in late '14 and early '15 using the v0.9.4 clients
-        i2pvSeeds.push_back(CDNSSeedData("a4gii55rnvv22qm2ojre2n67bzms5utr4k3ckafwjdoym2cqmv2q.b32.i2p", "a4gii55rnvv22qm2ojre2n67bzms5utr4k3ckafwjdoym2cqmv2q.b32.i2p")); // K1773R's seednode
-        i2pvSeeds.push_back(CDNSSeedData("b7ziruwpk7g2e44xyomnc2nu5tx7bc2f2ai4dzi66uxm3bc3qttq.b32.i2p", "b7ziruwpk7g2e44xyomnc2nu5tx7bc2f2ai4dzi66uxm3bc3qttq.b32.i2p")); // K1773R's seednode (dnsseed01)
-        i2pvSeeds.push_back(CDNSSeedData("72vaef5cmmlcilgvpeltcp77gutsnyic2l5khsgz7kyivla5lwjq.b32.i2p", "72vaef5cmmlcilgvpeltcp77gutsnyic2l5khsgz7kyivla5lwjq.b32.i2p")); // riddler's seednode
-        i2pvSeeds.push_back(CDNSSeedData("qc37luxnbh3hkihxfl2e7nwosebh5sbfvpvjqwn7c3g5kqftb5qq.b32.i2p", "qc37luxnbh3hkihxfl2e7nwosebh5sbfvpvjqwn7c3g5kqftb5qq.b32.i2p")); // psi's seednode
-        i2pvSeeds.push_back(CDNSSeedData("xynjl64xlviqhkjl2fbvupj7y3wct46jtayoxm2ksba6tqzo6tsa.b32.i2p", "xynjl64xlviqhkjl2fbvupj7y3wct46jtayoxm2ksba6tqzo6tsa.b32.i2p")); // Cryptoslave's seednode
+        // As of March 30, 2015 these entries were not working.
+//        i2pvSeeds.push_back(CDNSSeedData("d46o5wddsdrvg2ywnu4o57zeloayt7oyg56adz63xukordgfommq.b32.i2p", "d46o5wddsdrvg2ywnu4o57zeloayt7oyg56adz63xukordgfommq.b32.i2p"));
+//        i2pvSeeds.push_back(CDNSSeedData("u2om3hgjpghqfi7yid75xdmvzlgjybstzp6mtmaxse4aztm23rwq.b32.i2p", "u2om3hgjpghqfi7yid75xdmvzlgjybstzp6mtmaxse4aztm23rwq.b32.i2p"));
+//        i2pvSeeds.push_back(CDNSSeedData("htigbyeisbqizn63ftqw7ggfwfeolwkb3zfxwmyffygbilwqsswq.b32.i2p", "htigbyeisbqizn63ftqw7ggfwfeolwkb3zfxwmyffygbilwqsswq.b32.i2p"));
+//        i2pvSeeds.push_back(CDNSSeedData("st4eyxcp73zzbpatgt26pt3rlfwb7g5ywedol65baalgpnhvzqpa.b32.i2p", "st4eyxcp73zzbpatgt26pt3rlfwb7g5ywedol65baalgpnhvzqpa.b32.i2p"));
+//        i2pvSeeds.push_back(CDNSSeedData("qgmxpnpujddsd5ez67p4ognqsvo64tnzdbzesezdbtb3atyoxcpq.b32.i2p", "qgmxpnpujddsd5ez67p4ognqsvo64tnzdbzesezdbtb3atyoxcpq.b32.i2p"));
+//        i2pvSeeds.push_back(CDNSSeedData("a4gii55rnvv22qm2ojre2n67bzms5utr4k3ckafwjdoym2cqmv2q.b32.i2p", "a4gii55rnvv22qm2ojre2n67bzms5utr4k3ckafwjdoym2cqmv2q.b32.i2p")); // K1773R's seednode
+//        i2pvSeeds.push_back(CDNSSeedData("72vaef5cmmlcilgvpeltcp77gutsnyic2l5khsgz7kyivla5lwjq.b32.i2p", "72vaef5cmmlcilgvpeltcp77gutsnyic2l5khsgz7kyivla5lwjq.b32.i2p")); // riddler's seednode
+//        i2pvSeeds.push_back(CDNSSeedData("qc37luxnbh3hkihxfl2e7nwosebh5sbfvpvjqwn7c3g5kqftb5qq.b32.i2p", "qc37luxnbh3hkihxfl2e7nwosebh5sbfvpvjqwn7c3g5kqftb5qq.b32.i2p")); // psi's seednode
+//        i2pvSeeds.push_back(CDNSSeedData("xynjl64xlviqhkjl2fbvupj7y3wct46jtayoxm2ksba6tqzo6tsa.b32.i2p", "xynjl64xlviqhkjl2fbvupj7y3wct46jtayoxm2ksba6tqzo6tsa.b32.i2p")); // Cryptoslave's seednode
 #endif // ENABLE_I2PSAM
 
         // Because the prefix bytes are attached to a base256 encoding (ie, binary) of known width,  they affect the leading cinquantoctal digit of the corresponding base58
@@ -136,32 +143,21 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
 
-        // Convert the pnSeeds array into usable address objects.
-        // ToDo: Generate some usable ip (IPv4) addresses for the pnSeed array  See: contrib/seeds/ for details...
-        // Or Not: ...as we migrate to I2P for everything, this becomes less of a feature we even care about.
-        // WARNING - Because there are no clearnet seeds, there is a line of code to not load fixed entries unless i2p is enabled.
-        for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
-        {
-            // Only connect to one or two seed nodes because once it connects,
-            // it will get a pile of addresses with newer timestamps. So Seed
-            // nodes are given a random 'last seen time' of between one and two weeks ago.
-            const int64_t nOneWeek = 7*24*60*60;
-            struct in_addr ip;
-            memcpy(&ip, &pnSeed[i], sizeof(ip));
-            CAddress addr(CService(ip, GetDefaultPort()));
-            addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
-            vFixedSeeds.push_back(addr);
-        }
 #ifdef ENABLE_I2PSAM
         // As of 12/26/2014, there are NO entries for the above clearnet pnSeed array,
         // only using I2P for fixed seeding now, the strings are base64 encoded I2P
         // Destination addresses, and we set the port to 0.
-        for (unsigned int i = 0; i < ARRAYLEN( I2pSeedAddresses ); i++ ) {
+        for (unsigned int i = 0; i < ARRAYLEN( I2pDestinationSeeds ); i++ ) {
             const int64_t nOneWeek = 7*24*60*60;
             // Fixed seed nodes get our standard services bits set, this is after creating a CService obj with port 0,
             // and a CNetAddr obj, where setspecial is called given the I2P Destination string so it is setup correctly
             // with our new GarlicCat value for routing...
-            CAddress addr( CService( CNetAddr( I2pSeedAddresses[i] ), 0 ), NODE_NETWORK | NODE_BLOOM | NODE_I2P );
+            //
+            // ToDo: Change i2p fixed seed node details to also include the services they support.
+            // The seed nodes should be added with the correct value, if that information does not
+            // matching in addrman, it means creating double entries, when the node is connected,
+            // and the correct values found out after the peer version details have been learned.
+            CAddress addr( CService( CNetAddr( I2pDestinationSeeds[i] ), 0 ), NODE_NETWORK | NODE_I2P );
             addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
             vFixedSeeds.push_back(addr);
         }
