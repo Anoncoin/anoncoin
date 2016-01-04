6ba740fd6740e68b5e3ece268e3979a143755f73
Cryptoslave <cryptoslave@anoncoin.net>
2016-01-04 20:49:44 +0100

    uint256 in addrman

--- src/addrman.h
+++ src/addrman.h
@@ -19,9 +19,11 @@
 #include "chainparams.h"
 #include "netbase.h"
 #include "protocol.h"
+#include "random.h"
 #include "serialize.h"
 #include "sync.h"
 #include "timedata.h"
+#include "uint256.h"
 #include "util.h"
 
 #include <map>
@@ -108,13 +110,13 @@ public:
     }
 
     //! Calculate in which "tried" bucket this entry belongs
-    int GetTriedBucket(const std::vector<unsigned char> &nKey) const;
+    int GetTriedBucket(const uint256 &nKey) const;
 
     //! Calculate in which "new" bucket this entry belongs, given a certain source
-    int GetNewBucket(const std::vector<unsigned char> &nKey, const CNetAddr& src) const;
+    int GetNewBucket(const uint256 &nKey, const CNetAddr& src) const;
 
     //! Calculate in which "new" bucket this entry belongs, using its default source
-    int GetNewBucket(const std::vector<unsigned char> &nKey) const
+    int GetNewBucket(const uint256 &nKey) const
     {
         return GetNewBucket(nKey, source);
     }
@@ -230,7 +232,7 @@ private:
     mutable CCriticalSection cs;
 
     //! secret key to randomize bucket select with
-    std::vector<unsigned char> nKey;
+    uint256 nKey;
 
     //! last used nId
     int nIdCount;
@@ -354,6 +356,7 @@ public:
 
         unsigned char nVersion = 0;
         s << nVersion;
+		s << ((unsigned char)32);
         s << nKey;
         s << nNew;
         s << nTried;
@@ -398,6 +401,9 @@ public:
 
         unsigned char nVersion;
         s >> nVersion;
+		unsigned char nKeySize;
+		s >> nKeySize;
+		if (nKeySize != 32) throw std::ios_base::failure("Incorrect keysize in addrman");
         s >> nKey;
         s >> nNew;
         s >> nTried;
@@ -508,13 +514,18 @@ public:
 
     CAddrMan() : vRandom(0), vvTried(ADDRMAN_TRIED_BUCKET_COUNT, std::vector<int>(0)), vvNew(ADDRMAN_NEW_BUCKET_COUNT, std::set<int>())
     {
-         nKey.resize(32);
-         RAND_bytes(&nKey[0], 32);
+         nKey = GetRandHash();
 
          nIdCount = 0;
          nTried = 0;
          nNew = 0;
     }
+
+	~CAddrMan()
+	{
+	nKey.SetNull();
+	}
+
 
     //! Return the number of (unique) addresses in all tables.
     int size()
