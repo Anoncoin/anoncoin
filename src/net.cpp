d86131d868d14dda8a64a5c1e1133e38613ba494
Scott Switzer <switzer@switzer.org>
2018-02-20 21:46:50 -0500

    added return value for error case to get rid of warning:  net.cpp:2084:1: warning: control may reach end of non-void function [-Wreturn-type]

--- src/net.cpp
+++ src/net.cpp
@@ -1397,19 +1397,18 @@ void ThreadMapPort()
     struct UPNPDev * devlist = 0;
     char lanaddr[64];
 
-    #ifndef UPNPDISCOVER_SUCCESS
-        /* miniupnpc 1.5 */
-        devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-    #elif MINIUPNPC_API_VERSION < 14
-        /* miniupnpc 1.6 */
-        int error = 0;
-        devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
-    #else
-        /* miniupnpc 1.9.20150730 */
-        int error = 0;
-        devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
-    #endif
-
+#ifndef UPNPDISCOVER_SUCCESS
+    /* miniupnpc 1.5 */
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
+#elif MINIUPNPC_API_VERSION < 14
+    /* miniupnpc 1.6 */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+#else
+    /* miniupnpc 1.9.20150730 */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
+#endif
 
     struct UPNPUrls urls;
     struct IGDdatas data;
@@ -2082,6 +2081,7 @@ bool BindListenNativeI2P(SOCKET& hSocket)
     else
         LogPrintf( "ERROR - Unexpected I2P BIND request. Ignored, network access is limited.\n" );
 
+    return false;
 }
 #endif // ENABLE_I2PSAM
 
